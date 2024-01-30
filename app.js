9fad7ba5cecaba8dafa54fd3eb08a7dbca08970b
Olga <minaeva9@gmail.com>
2024-01-30 18:26:08 -0500

    css modified

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const colorSection = document.getElementById("color-section");
-let colorInp = document.getElementById("color-picker");
-let colorMode = document.getElementById("color-mode");
 
 function getColors() {
-  colorInp = colorInp.value.substr(-6);
-  colorMode = colorMode.value;
+  let colorInp = document.getElementById("color-picker").value.substr(-6);
+  let colorMode = document.getElementById("color-mode").value;
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${colorInp}&mode=${colorMode}&count=5`
   )
@@ -15,7 +13,7 @@ function getColors() {
         html += `
             <div class="color-value">
               <img class="color-img" src="${color.image.bare}">
-              <h3>${color.hex.value}</h3>
+              <h3 id="name">${color.hex.value}</h3>
             </div>`;
       }
       colorSection.innerHTML = html;
